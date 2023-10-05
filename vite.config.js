42e3b3976e438e47512e9402db40f98b0b197207
Sahad M <sahadm040@gmail.com>
2023-10-05 10:34:45 +0530

    an change 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3040, // Change this to the desired port number
+  },
 })
